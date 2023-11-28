9759357b6f8a71a73e7da442566d5daecf6634ef
MrSabin <sabin.21011986@gmail.com>
2023-11-28 21:57:07 +0300

    Fix brackets

--- sb-deploy.sh
+++ sb-deploy.sh
@@ -22,6 +22,6 @@ last_commit=$(git rev-parse HEAD)
 curl -H "X-Rollbar-Access-Token: ${ROLLBAR_TOKEN}" \
      -H "Content-Type: application/json" \
      -X POST 'https://api.rollbar.com/api/1/deploy' \
-     -d '{"environment": "production", "revision": "${last_commit}", "rollbar_username": "${ROLLBAR_USERNAME}", "status": "succeeded"}'
+     -d '{"environment": "production", "revision": "'${last_commit}'", "rollbar_username": "'${ROLLBAR_USERNAME}'", "status": "succeeded"}'
 
 echo Updated
