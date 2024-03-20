3c0611b0fd8ea76f86d2de3239c757bcd8f6699f
MrSabin <sabin.21011986@gmail.com>
2024-03-20 22:25:27 +0300

    Corrected deploy script

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -4,7 +4,5 @@ set -Eeuo pipefail
 
 cd /opt/star-burgers
 export $(xargs < ./star_burger/.env)
-docker-compose build
-docker-compose pull
-docker-compose up -d
+docker-compose up -d --build
 echo "Deploy succesful"
